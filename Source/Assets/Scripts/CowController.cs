9f8777c59430fa032e7439a040524da1beee8e4d
Wyzzus <wyzzus@gmail.com>
2020-03-20 08:38:13 +0300

    Make main and stego mechanics

---
+++ Source/Assets/Scripts/CowController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class CowController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Grass currentGrass;
+
+    public GameObject[] Flowers;
+
+    public string[] Messages;
+
+    public Material Red;
+    public Material Blue;
+
+    float timer = 8f;
+    public float currentTimer = 8;
+    public float MooTimer = 2f;
+    public int currentIndex;
+    public Transform Moo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void HandleFlowers(int j)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (Messages[j][i] == '0')
+                Flowers[i].GetComponentInChildren<Renderer>().material = Red;
+            else
+                Flowers[i].GetComponentInChildren<Renderer>().material = Blue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Movement();
+        }
+
+        Moo.rotation = Camera.main.transform.rotation;
+
+        if (currentTimer < timer)
+            currentTimer += Time.deltaTime;
+        else
+        {
+            HandleFlowers(currentIndex);
+            currentIndex++;
+            currentTimer = 0;
+            MooTimer = 2f;
+            Moo.gameObject.SetActive(true);
+        }
+
+        if (MooTimer > 0)
+            MooTimer -= Time.deltaTime;
+        else
+        {
+            Moo.gameObject.SetActive(false);
+        }
+
+
+        if (currentIndex >= Messages.Length)
+        {
+            currentIndex = 0;
+        }
+        if (currentIndex == Messages.Length - 1)
+        {
+            Moo.GetComponentInChildren<Text>().text = "MOOO!";
+        }
+        if (currentIndex < Messages.Length - 1)
+        {
+            Moo.GetComponentInChildren<Text>().text = "MOO!";
+        }
+
+        if (currentGrass)
+        {
+            if (Vector3.Distance(transform.position, currentGrass.transform.position) < 0.5f)
+            {
+                currentGrass.Eat();
+                Debug.Log("Eated " + currentGrass.name);
+                currentGrass = null;
+            }
+        }
+    }
+
+    public void Movement()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            currentGrass = hit.collider.GetComponent<Grass>();
+            agent.SetDestination(hit.point);
+        }
+    }
+}
---
+++ Source/Assets/Scripts/Grass.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    float timer = 4f;
+    public float currentTimer = 4;
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTimer < timer)
+        {
+            currentTimer += Time.deltaTime;
+        }
+        else
+        {
+            anim.SetBool("Growed", true);
+        }
+    }
+
+    public void Eat()
+    {
+        anim.SetBool("Growed", false);
+        currentTimer = 0;
+    }
+}
